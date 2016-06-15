77ae7ed4c9cdbb34baba3acffcea8111d2147141
lumin <lumin824@163.com>
2016-06-15 13:06:31 +0800

    move codepush

--- js/action/index.js
+++ js/action/index.js
@@ -2,9 +2,7 @@ export * from './login';
 export * from './device';
 
 import api from './api';
-import codePush from './codePush';
 
 export default {
   ...api,
-  ...codePush
 }
--- js/view/AboutView.js
+++ js/view/AboutView.js
@@ -18,9 +18,6 @@ class V extends Component {
     this.state = {};
   }
   componentDidMount(){
-    codePush.checkForUpdate().then(e=>console.log(e));
-
-    codePush.getUpdateMetadata().then(e=>console.log(e));
   }
   render(){
     return (
@@ -28,31 +25,11 @@ class V extends Component {
 
 
         <TextInput style={{height:40}}/>
-        <Text >{this.state.text}</Text>
+        <Text >1111</Text>
 
-        <TouchableOpacity style={{borderBottomWidth:1, flexDirection:'row', height:40}}>
-          <Text>软件版本12{__DEV__?'1':'2'}</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={()=>this.props.action.getCurrentPackage().then(e=>console.log(e))} style={{borderBottomWidth:1, flexDirection:'row', height:40}}>
-          <Text>数据版本</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={()=>this.props.action.checkForUpdate().then(e=>console.log(e))} style={{borderBottomWidth:1, flexDirection:'row', height:40}}>
-          <Text>检查</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={()=>this.props.action.sync().then(e=>console.log(e))} style={{borderBottomWidth:1, flexDirection:'row', height:40}}>
-          <Text>更新</Text>
-        </TouchableOpacity>
       </View>);
   }
 }
 
-export default connect(state=>state,dispatch=>({
-  action: bindActionCreators({
-    checkForUpdate: action.checkForUpdate,
-    getCurrentPackage: action.getCurrentPackage,
-    sync: action.sync
-  }, dispatch)
-}))(V);
+export default connect(state=>state)(V);
